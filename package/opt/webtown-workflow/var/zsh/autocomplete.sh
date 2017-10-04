e7eb756591abf6fc509b66711a2ff779621afcab
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2017-10-04 12:20:46 +0200

    Add debug-enter + upgrade enter command

--- package/opt/webtown-workflow/var/zsh/autocomplete.sh
+++ package/opt/webtown-workflow/var/zsh/autocomplete.sh
@@ -16,8 +16,11 @@ _wf() {
                 feature | hotfix)
                     _arguments '*: :(--from-this --disable-db --reload-d)'
                 ;;
-                connect | exec | run | logs)
-                    _arguments '2: :($(wf docker-compose config --services))' '3: :(/bin/bash)'
+                connect | enter | debug-enter | logs)
+                    _arguments '2: :($(wf docker-compose config --services))'
+                ;;
+                exec | run)
+                    _arguments '*: :($(wf docker-compose config --services))'
                 ;;
             esac
         ;;
