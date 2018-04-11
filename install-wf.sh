ab560bf37a89ccd678a3473d185afd97bcd77c1f
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-04-11 20:58:53 +0200

    Debug fix

--- install-wf.sh
+++ install-wf.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ${DEBUG:-0} -ge 1 ]; then
+    [[ -f /.dockerenv ]] && echo -e "\033[1mDocker: \033[33m${WF_DOCKER_HOST_CHAIN}\033[0m"
+    echo -e "\033[1mDEBUG\033[33m $(realpath "$0")\033[0m"
+    SYMFONY_COMMAND_DEBUG="-vvv"
+    DOCKER_DEBUG="-e DEBUG=${DEBUG}"
+fi
+[[ ${DEBUG:-0} -ge 2 ]] && set -x
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -7,8 +15,6 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-set -x
 
 # Colors
 RED=$'\x1B[00;31m'
