cc765ba170bf155df6340cc267fb36e8bce86073
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-04-20 15:23:21 +0200

    Add gitlab_ci_webtown_runner recipe

---
+++ webtown-workflow-package/opt/webtown-workflow/recipes/GitlabCiWebtownRunner/Recipe.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Recipes\GitlabCiWebtownRunner;
+
+use App\Skeleton\DockerComposeSkeletonFile;
+use Recipes\GitlabCi\Recipe as BaseRecipe;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Finder\SplFileInfo;
+
+class Recipe extends BaseRecipe
+{
+    const NAME = 'gitlab_ci_webtown_runner';
+
+    public function getName()
+    {
+        return static::NAME;
+    }
+
+    public static function getSkeletonParents()
+    {
+        return [BaseRecipe::class];
+    }
+
+    public function getConfig()
+    {
+        $rootNode = parent::getConfig();
+
+        /**
+         *  gitlab_ci_webtown_runner:
+         *      share_home_with: engine
+         *      volumes:
+         *          mysql:
+         *              data: /usr/mysql/data
+         */
+
+        $rootNode
+            ->info('<comment>GitLab CI Webtown Runner</comment>')
+            ->children()
+                ->arrayNode('share_home_with')
+                    ->info('<comment>Share composer cache or other things between tests. Service name list.</comment>')
+                    ->scalarPrototype()->end()
+                    ->defaultValue(['engine'])
+                ->end()
+                ->arrayNode('volumes')
+                    ->info('<comment>Register template volumes.</comment>')
+                    ->useAttributeAsKey('service')
+                    ->variablePrototype()
+                        ->beforeNormalization()
+                            ->ifString()
+                            ->then(function ($v) {
+                                return ['data' => $v];
+                            })
+                            ->end()
+                        ->end()
+                        ->validate()
+                            ->always(function ($v) {
+                                foreach ($v as $name => $target) {
+                                    if (!is_string($name)) {
+                                        throw new InvalidConfigurationException(sprintf('You have to use string key in `%s`.volumes', static::NAME));
+                                    }
+                                }
+
+                                return $v;
+                            })
+                        ->end()
+                        ->example([
+                            'service1' => '/usr/mysql/data',
+                            'service2' => ['data' => '/usr/mysql/data', 'config' => '/usr/mysql/config'],
+                        ])
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $rootNode;
+    }
+
+    protected function buildSkeletonFile(SplFileInfo $fileInfo, $config)
+    {
+        // share_home_with settings
+        if ($fileInfo->getFilename() == 'docker-compose.home.yml' && isset($config['share_home_with']) && count($config['share_home_with']) > 0) {
+            return new DockerComposeSkeletonFile($fileInfo);
+        }
+        // volumes settings
+        if ($fileInfo->getFilename() == 'docker-compose.volumes.yml' && isset($config['volumes']) && count($config['volumes']) > 0) {
+            return new DockerComposeSkeletonFile($fileInfo);
+        }
+
+        return parent::buildSkeletonFile($fileInfo, $config);
+    }
+}
--- webtown-workflow-package/opt/webtown-workflow/symfony4/src/Command/ConfigYamlDumpCommand.php
+++ webtown-workflow-package/opt/webtown-workflow/symfony4/src/Command/ConfigYamlDumpCommand.php
@@ -3,10 +3,15 @@
 namespace App\Command;
 
 use App\Configuration\Configuration;
+use App\Configuration\RecipeManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Config\Definition\ArrayNode;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\Dumper\YamlReferenceDumper;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -24,6 +29,7 @@ class ConfigYamlDumpCommand extends ContainerAwareCommand
             ->setName('app:config-dump')
             ->setDescription('Project config dump. Use the <comment>--no-ansi</comment> argument if you want to put it into a file!')
             ->setHelp('Use the <info>--no-ansi</info> argument if you want to put it into a file!')
+            ->addOption('recipe', null, InputOption::VALUE_OPTIONAL, 'You have to chose a recipe', null)
         ;
     }
 
@@ -35,8 +41,6 @@ class ConfigYamlDumpCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $configuration = $this->getContainer()->get(Configuration::class);
-        $dumper = new YamlReferenceDumper();
 
         // We show it if the user don't want to put it into a file!
         if ($io->isDecorated()) {
@@ -44,11 +48,42 @@ class ConfigYamlDumpCommand extends ContainerAwareCommand
             $io->writeln('  Use the <info>--no-ansi</info> argument if you want to put it into a file!');
         }
 
-        /** @var ArrayNode $rootNode */
-        $rootNode = $configuration->getConfigTreeBuilder()->buildTree();
-        // Show only the children
-        foreach ($rootNode->getChildren() as $node) {
-            $io->write($dumper->dumpNode($node));
+        $dumper = new YamlReferenceDumper();
+
+        $baseConfiguration = $this->getContainer()->get(Configuration::class);
+        if ($recipeNameOrClass = $input->getOption('recipe')) {
+            $recipe = $this->getRecipeByNameOrClass($recipeNameOrClass);
+            /** @var ArrayNode $rootNode */
+            $rootNode = $recipe->getConfig();
+            $io->write($dumper->dumpNode($rootNode->getNode(true)));
+        } else {
+            /** @var ArrayNode $rootNode */
+            $rootNode = $baseConfiguration->getConfigTreeBuilder()->buildTree();
+            // Show only the children
+            foreach ($rootNode->getChildren() as $node) {
+                $io->write($dumper->dumpNode($node));
+            }
         }
+    }
+
+    /**
+     * @param string $nameOrClass
+     *
+     * @return \Recipes\BaseRecipe
+     */
+    protected function getRecipeByNameOrClass($nameOrClass)
+    {
+        $recipeManager = $this->getContainer()->get(RecipeManager::class);
+        $altFqn = sprintf('Recipes\%s\Recipe', $nameOrClass);
+        foreach ($recipeManager->getRecipes() as $recipe) {
+            if (get_class($recipe) == $nameOrClass
+                || get_class($recipe) == $altFqn
+                || $recipe->getName() == $nameOrClass
+            ) {
+                return $recipe;
+            }
+        }
+
+        throw new InvalidArgumentException(sprintf('Missing or wrong recipe: `%s`', $nameOrClass));
     }
 }
