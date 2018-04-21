8e108a181e5a36b3900f814693d4411b60cc7460
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-04-21 14:11:26 +0200

    Fix gitflow recipe

--- webtown-workflow-package/opt/webtown-workflow/recipes/GitFlow/Recipe.php
+++ webtown-workflow-package/opt/webtown-workflow/recipes/GitFlow/Recipe.php
@@ -26,6 +26,39 @@ class Recipe extends BaseRecipe
         return static::NAME;
     }
 
+
+    public function getConfig()
+    {
+        $rootNode = parent::getConfig();
+
+        /**
+         *  git_flow:
+         *      develop: dev
+         *      feature: f
+         *      hotfix:  h
+         */
+        $rootNode
+            ->info('<comment>GitFlow</comment>')
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('develop')
+                    ->info('<comment>Develop branch name</comment>')
+                    ->defaultValue('develop')
+                ->end()
+                ->scalarNode('feature')
+                    ->info('<comment>Feature branches prefix</comment>')
+                    ->defaultValue('feature')
+                ->end()
+                ->scalarNode('hotfix')
+                    ->info('<comment>Hotfix branches prefix</comment>')
+                    ->defaultValue('hotfix')
+                ->end()
+            ->end()
+        ;
+
+        return $rootNode;
+    }
+
     public function getTemplateVars($targetPath, $recipeConfig, $globalConfig)
     {
         $vars = parent::getTemplateVars($targetPath, $recipeConfig, $globalConfig);
