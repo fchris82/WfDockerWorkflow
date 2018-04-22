5807da588d2d7ab2f0067aa8c231e859339d2ef1
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-04-22 23:15:59 +0200

    [WIP] change to alpine linux

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,24 +9,18 @@ fi
 [[ ${DEBUG:-0} -ge 2 ]] && set -x
 
 if [ "${XDEBUG_ENABLED}" == "1" ]; then
-    mkdir -p /tmp/php.conf.d
-    echo "xdebug.remote_host = $(/sbin/ip route|awk '/default/ { print $3 }')" > /tmp/php.conf.d/xdebug.ini
-    export PHP_INI_SCAN_DIR=$PHP_INI_DIR/conf.d:/tmp/php.conf.d
+    echo "xdebug.remote_host = $(/sbin/ip route|awk '/default/ { print $3 }')" > ${XDEBUG_CONFIG_FILE}
+    mv ${XDEBUG_CONFIG_FILE} ${XDEBUG_CONFIG_FILE%.*}
     export XDEBUG_CONFIG="idekey=Docker"
     export PHP_IDE_CONFIG="serverName=Docker"
-else
-    # Disable xdebug
-    XDEBUG_INI_BASE=`php --ini | grep -oh "\S*xdebug.ini"`
-    XDEBUG_INI=$([ -h ${XDEBUG_INI_BASE} ] && readlink ${XDEBUG_INI_BASE} || echo ${XDEBUG_INI_BASE})
-    mv ${XDEBUG_INI} ${XDEBUG_INI}.disable
 fi
 
 # USER
 USER_ID=${LOCAL_USER_ID:-9001}
-useradd -u $USER_ID ${LOCAL_USER_NAME} -G docker
+adduser -u $USER_ID -D -S -H ${LOCAL_USER_NAME} -G docker
 export HOME=${LOCAL_USER_HOME}
 
 [[ -f /opt/webtown-workflow/symfony4/.env ]] && chown -R ${USER_ID} /opt/webtown-workflow/symfony4/.env
 [[ -f /opt/webtown-workflow/symfony4/var ]] && chown -R ${USER_ID} /opt/webtown-workflow/symfony4/var
 
-gosu ${LOCAL_USER_NAME} "$@"
+su-exec ${LOCAL_USER_NAME} "$@"
