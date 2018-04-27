4149b8d25ce22baf4624d74c5621b383f36832fb
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-04-27 17:03:01 +0200

    Fix the symlink config file

--- webtown-workflow-package/opt/webtown-workflow/host/bin/workflow_runner.sh
+++ webtown-workflow-package/opt/webtown-workflow/host/bin/workflow_runner.sh
@@ -114,10 +114,17 @@ if [ "${CI}" == "0" ] && [ -t 1 ]; then
     TTY="-it"
 fi
 
+# If the .wf.yml is a symlink, we put it into directly. It happens forexample if you are using deployer on a server, and
+# share this file among different versions.
+if [ -L ${WORKDIR}/${WF_WORKING_DIRECTORY_NAME} ]; then
+    CONFIG_FILE_SHARE="$(readlink -f ${WORKDIR}/${WF_WORKING_DIRECTORY_NAME}):${WORKDIR}/${WF_WORKING_DIRECTORY_NAME}"
+fi;
+
 docker run ${TTY} \
             ${DOCKER_COMPOSE_ENV} \
             -w ${WORKDIR} \
             ${WORKDIR_SHARE} \
+            ${CONFIG_FILE_SHARE:''} \
             -v ${RUNNER_HOME:-$HOME}:${HOME} \
             -v /var/run/docker.sock:/var/run/docker.sock \
             ${DOCKER_DEVELOP_PATH_VOLUME} \
