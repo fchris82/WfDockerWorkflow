6dd60d010e70106aa2c87a7f381c1b2db10a33d6
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-06-04 13:37:44 +0200

    Bugfix and generate new deb

--- webtown-workflow-package/opt/webtown-workflow/lib/_functions.sh
+++ webtown-workflow-package/opt/webtown-workflow/lib/_functions.sh
@@ -129,7 +129,7 @@ function create_makefile_from_config {
         ${DIR}/../webtown-workflow/wizard.sh --reconfigure \
             --file ${PROJECT_CONFIG_FILE} \
             --target-directory ${WF_WORKING_DIRECTORY_NAME} \
-            --config-hash ${CONFIG_HASH} ${@} || quit
+            --config-hash ${CONFIG_HASH}.${WF_VERSION} ${@} || quit
     fi
 }
 
