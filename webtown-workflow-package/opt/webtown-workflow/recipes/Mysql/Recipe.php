e9761b9c3e92d55dc95889d58c24c5d6791fdabe
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-10-17 22:07:52 +0200

    Upgrade Mysql recipe
    Enable multiple database option

--- webtown-workflow-package/opt/webtown-workflow/recipes/Mysql/Recipe.php
+++ webtown-workflow-package/opt/webtown-workflow/recipes/Mysql/Recipe.php
@@ -28,28 +28,88 @@ class Recipe extends BaseRecipe
         $rootNode
             ->info('<comment>Include a MySQL service</comment>')
             ->children()
-                ->scalarNode('version')
-                    ->info('<comment>Docker image tag</comment>')
-                    ->isRequired()
-                    ->cannotBeEmpty()
+                ->arrayNode('defaults')
+                    ->info('<comment>You can set some defaults for all containers!</comment>')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('version')
+                            ->info('<comment>Docker image tag</comment>')
+                        ->end()
+                        ->scalarNode('password')
+                            ->info('<comment>The <info>root</info> password.</comment>')
+                        ->end()
+                        ->booleanNode('local_volume')
+                            ->info('<comment>You can switch the using local volume.</comment>')
+                        ->end()
+                    ->end()
                 ->end()
-                ->scalarNode('database')
-                    ->info('<comment>Database name</comment>')
-                    ->isRequired()
-                    ->cannotBeEmpty()
+                ->arrayNode('databases')
+                    ->info('<comment>Configuration of the MySql containers.</comment>')
+                    ->useAttributeAsKey('docker_container_name')
+                    ->prototype('array')
+                        ->info('<comment>The docker container name. You have to link through this! Eg: <info>mysql -u root -p -h [docker_container_name]</info>.</comment>')
+                        ->addDefaultsIfNotSet()
+                        ->children()
+                            ->scalarNode('version')
+                                ->info('<comment>Docker image tag (you can define default for all, see <info>mysql.defaults.version</info>)</comment>')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->scalarNode('database')
+                                ->info('<comment>Database name</comment>')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->scalarNode('password')
+                                ->info('<comment>The <info>root</info> password. (you can define default for all, see <info>mysql.defaults.password</info>)</comment>')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->booleanNode('local_volume')
+                                ->info('<comment>You can switch the using local volume.</comment>')
+                                ->defaultTrue()
+                            ->end()
+                            ->integerNode('port')
+                                ->info('<comment>If you want to enable this container from outside set the port number.</comment>')
+                                ->defaultValue(0)
+                            ->end()
+                        ->end()
+                    ->end()
                 ->end()
-                ->scalarNode('password')
-                    ->info('<comment>The <info>root</info> password.</comment>')
-                    ->isRequired()
-                    ->cannotBeEmpty()
-                ->end()
-                ->booleanNode('local_volume')
-                    ->info('<comment>You can switch the using local volume.</comment>')
-                    ->defaultTrue()
-                ->end()
-                ->integerNode('port')
-                    ->info('<comment>If you want to enable this container from outside set the port number.</comment>')
-                    ->defaultValue(0)
+            ->end()
+            ->beforeNormalization()
+                ->always(function($v) {
+                    if (is_array($v)) {
+                        // Backward compatibility
+                        if (!array_key_exists('databases', $v)) {
+                            $w['databases']['mysql'] = $v;
+                            $v = $w;
+                        // Handle defaults
+                        } elseif (array_key_exists('defaults', $v) && is_array($v['defaults'])) {
+                            foreach ($v['defaults'] as $key => $defaultValue) {
+                                foreach ($v['databases'] as $dockerContainerName => $config) {
+                                    // If the config empty, then we use only defaults
+                                    if (!$config) {
+                                        $config = [
+                                            'database' => $dockerContainerName . '_db',
+                                        ];
+                                    } elseif (!is_array($config)) {
+                                        throw new \InvalidArgumentException(sprintf(
+                                            'Invalid configuration value in the <info>mysql.databases.%s</info> place. You have to use array or null instead of %s',
+                                            $dockerContainerName,
+                                            gettype($config)
+                                        ));
+                                    }
+                                    if (!array_key_exists($key, $config) || (!$config[$key] && $config[$key] !== false)) {
+                                        $v['databases'][$dockerContainerName][$key] = $defaultValue;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    return $v;
+                })
                 ->end()
             ->end()
         ;
