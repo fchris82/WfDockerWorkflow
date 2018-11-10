064a5bf8726d0d4cf6e1f848f2b7af89a731d889
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-11-10 11:23:30 +0100

    Autocomplete upgrade

--- webtown-workflow-package/opt/webtown-workflow/recipes/Symfony/skeletons/autocomplete.zsh
+++ webtown-workflow-package/opt/webtown-workflow/recipes/Symfony/skeletons/autocomplete.zsh
@@ -1,11 +1,11 @@
-local sf_cache_file=${wf_directory_name}/autocomplete.sf
-[[ ! -f $sf_cache_file ]] || [[ -z $(cat $sf_cache_file) ]] && wf sf | egrep -o "^  [a-z]+:[^ ]+" | egrep -o "[^ ]+" > $sf_cache_file
-local sf_cache_commands=$(<$sf_cache_file)
-
 case $state in
     parameters)
         case $words[2] in
             sf)
+                local sf_cache_file=${wf_directory_name}/autocomplete.sf
+                [[ ! -f $sf_cache_file ]] || [[ -z $(cat $sf_cache_file) ]] && wf sf | egrep -o "^  [a-z]+:[^ ]+" | egrep -o "[^ ]+" > $sf_cache_file
+                local sf_cache_commands=$(<$sf_cache_file)
+
                 _arguments '2: :($(echo ${sf_cache_commands:-""}))'
             ;;
         esac
