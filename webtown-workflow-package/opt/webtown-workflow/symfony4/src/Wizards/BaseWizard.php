d9282a91e3d1d9e57b614683059f4f5c7eeca3fe
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-11-30 11:25:09 +0100

    Fix live execute command

--- webtown-workflow-package/opt/webtown-workflow/symfony4/src/Wizards/BaseWizard.php
+++ webtown-workflow-package/opt/webtown-workflow/symfony4/src/Wizards/BaseWizard.php
@@ -230,16 +230,21 @@ abstract class BaseWizard implements WizardInterface
         );
 
         $this->output->writeln(sprintf('[exec] <comment>%s</comment>', $printedCmd));
-        passthru($cmd, $return);
-        if (0 == $return) {
+        $result = $this->liveExecuteCommand($cmd);
+        $return = $result['exit_status'];
+        $output = $result['output'];
+
+        if (0 === $return) {
             $this->output->writeln(sprintf('[<info>OK</info>] %s', $printedCmd));
         } else {
             $this->output->writeln(sprintf('[<error>ERROR</error> (%d)] %s', $return, $printedCmd));
         }
 
         if (\is_callable($handleReturn)) {
-            $handleReturn($return);
+            return $handleReturn($return, $output);
         }
+
+        return $output;
     }
 
     protected function runCmdInContainer($cmd, $workdir = null, $handleReturn = null)
@@ -288,6 +293,36 @@ abstract class BaseWizard implements WizardInterface
             $containerCmd,
             $workdir,
             $handleReturn
+        );
+    }
+
+    protected function liveExecuteCommand($cmd)
+    {
+
+        while (@ ob_end_flush()); // end all output buffers if any
+
+        $proc = popen("$cmd 2>&1 ; echo Exit status : $?", 'r');
+
+        $live_output     = "";
+        $complete_output = "";
+
+        while (!feof($proc))
+        {
+            $live_output     = fread($proc, 4096);
+            $complete_output = $complete_output . $live_output;
+            echo "$live_output";
+            @ flush();
+        }
+
+        pclose($proc);
+
+        // get exit status
+        preg_match('/[0-9]+$/', $complete_output, $matches);
+
+        // return exit status and intended output
+        return array (
+            'exit_status'  => intval($matches[0]),
+            'output'       => rtrim(str_replace("Exit status : " . $matches[0], '', $complete_output))
         );
     }
 
