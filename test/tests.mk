5e56ca5d3ce9a9b052b809ebf7f51939433b4989
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2018-12-30 10:51:52 +0100

    Gitlab CI tests fix

--- test/tests.mk
+++ test/tests.mk
@@ -1,33 +1,12 @@
-DOCKER_COMPOSE_FILE := $$(dirname $(abspath $(firstword $(MAKEFILE_LIST))))/docker-compose.yml
-TEST_PROJECT_PATH := /tmp/www
-
-define run
-docker-compose -f $(DOCKER_COMPOSE_FILE) run -v $(TEST_PROJECT_PATH):$(TEST_PROJECT_PATH) --rm test make -f /tmp/tests/$(1) TEST_PROJECT_PATH=$(TEST_PROJECT_PATH) $(2)
-endef
-
-all: functions base test.1.0.0 test.1.1.0 clean
-
-# @todo Ebben a repo-ban el kellene helyezni egy DEPLOY SSH kulcsot a workflow-test projekthez, mert a gitlab-nál nem vagy nehezen tudjuk megoldani a kulcs átadását a többszörösöen beágyazott docker container-eken keresztül. Addig csak így külön tesztelhető.
-gitlab: functions clean
-
-rebuild:
-	docker-compose -f $(DOCKER_COMPOSE_FILE) build --no-cache test
-
-init:
-	mkdir -p $(TEST_PROJECT_PATH)
+all: functions phpunit phpcsfix
 
 functions:
-	./test/tests/functions.sh
+	./test/functions.sh
 
-base:
-	$(call run,base.mk all)
+.PHONY: phpunit
+phpunit:
+	~/bin/wfdev wf --dev-run bin/phpunit
 
-test.1.0.0: init
-	$(call run,test.1.0.0.mk all clean)
-
-test.1.1.0: init
-	$(call run,test.1.1.0.mk all clean)
-
-# @todo ez nem mindig fut le, jogosultság hiba miatt
-clean:
-	rm -rf $(TEST_PROJECT_PATH)
+.PHONY: phpcsfix
+phpcsfix:
+	~/bin/wfdev wf --dev-run vendor/bin/php-cs-fixer fix --dry-run
