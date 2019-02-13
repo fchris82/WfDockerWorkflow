6fe94752346b84a914df9ca9396d0c0b81d54e19
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2019-02-13 14:39:15 +0100

    Add SymfonyEz2Extension recipe

---
+++ webtown-workflow-package/opt/webtown-workflow/symfony4/src/Recipes/WebtownSymfonyEz2Extension/SymfonyEz2ExtensionRecipe.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: chris
+ * Date: 2018.03.29.
+ * Time: 15:23
+ */
+
+namespace App\Recipes\WebtownSymfonyEz2Extension;
+
+use App\Recipes\Symfony3\Symfony3Recipe;
+use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
+
+class SymfonyEz2ExtensionRecipe extends Symfony3Recipe
+{
+    const NAME = 'symfony_ez2_extension';
+    const DEFAULT_VERSION = 'ez2';
+
+    public function getConfig()
+    {
+        $rootNode = parent::getConfig();
+
+        $definitions = $rootNode->getChildNodeDefinitions();
+        /** @var ScalarNodeDefinition $projectDirDefinition */
+        $projectDirDefinition = $definitions['project_dir'];
+        $projectDirDefinition->defaultValue('ez');
+
+        return $rootNode;
+    }
+}
