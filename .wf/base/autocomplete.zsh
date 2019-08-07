6a512cc4ea80394ff2d8df037da6959beacd1d39
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2019-08-07 19:02:46 +0200

    prepare release

---
+++ .wf/base/autocomplete.zsh
@@ -0,0 +1,23 @@
+# This is an auto generated file from `/home/chris/www/wf-docker-workflow/.wf.yml` config file! You shouldn't edit this.
+
+_base_init() {
+    local cache_services_file=${wf_directory_name}/autocomplete.services
+    [[ ! -f $cache_services_file ]] || [[ -z $(cat $cache_services_file) ]] && wf docker-compose config --services > $cache_services_file
+    services=$(<$cache_services_file)
+}
+
+# Create autocomplete services
+case $state in
+    parameters)
+        case $words[2] in
+            connect | enter | debug-enter | logs)
+                _base_init
+                _arguments '2: :($(echo ${services:-$(wf docker-compose config --services)}))'
+            ;;
+            exec | run | sudo-run | docker-compose)
+                _base_init
+                _arguments '*: :($(echo ${services:-$(wf docker-compose config --services)}))'
+            ;;
+        esac
+    ;;
+esac
